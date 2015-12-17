302583efb6353e007ce4010558473b77442ae988
rajani <medir@WMEDIRW7BLR.rpega.com>
2015-12-17 14:57:58 +0530

    added ajax handling code

--- app/js/myjs.js
+++ app/js/myjs.js
@@ -1,6 +1,30 @@
 $( document ).ready(function() {
     /* Adding event to "add record" btn */
-    $("#addrecord").on("click" , function(){
-      alert("add btn clicked");
+    $(".fa").on("click" , function(){
+      var dataaction
+      switch($(this).attr("id")){
+        case "addrecord":
+          dataaction="create";
+          break;
+        case "deleterecord":
+         dataaction="delete";
+         break;
+         case "fetchrecord":
+           dataaction="fetch";
+           break;
+         case "updaterecord":
+          dataaction="update";
+          break;
+      }
+     $.ajax({
+        url:"/performaction",
+        method:"POST",
+        data:{"action":dataaction}
+     }).success(function(msg){
+
+       $("#content").html(msg);
+     }).error(function(){
+       alert("Error");
+     });
     });
  });
--- server.js
+++ server.js
@@ -1,19 +1,56 @@
 process.env.NODE_ENV=  process.env.NODE_ENV || 'development';
-var http=require('http'),fs=require("./my_modules/fileserver");
+var http=require('http'),fs=require("./my_modules/fileserver") , url = require('url') ,qs=require("querystring");;
 var mongoose=require("./config/mongoose");
 var db=mongoose();
 http.createServer(function(req,res){
- if(req.url=="/"){
-   fs("/welcome.html",req,res);
-   //debugger;
-console.log(req);
-}else{
-   fs(req.url,req,res);
-}
+        var queryObject;
+  if(req.method=="POST"){
+     var queryData = "";
+    req.on('data', function(data) {
+            queryData += data;
+        });
+
+        req.on('end', function() {
+          queryObject = qs.parse(queryData);
+          buildResponse(req,res,queryObject);
+        });
+  }else{
+       queryObject = url.parse(req.url,true).query;
+      buildResponse(req,res,queryObject);
+  }
+
+
  /*  res.writeHead(200,{
     'Content-Type':'text/plain'
   });
   res.end("Hello World");*/
 
 }).listen(8181);
+function buildResponse(req,res,queryObject){
+  if(req.url=="/"){
+    fs("/welcome.html",req,res);
+    //debugger;
+}else if(req.url=="/performaction"){
+
+
+ res.writeHead(200,{
+   'Content-Type':'text/plain'
+ });
+
+   if(queryObject["action"]=="fetch"){
+          res.write("Fetch");
+   }else if(queryObject["action"]=="create"){
+    res.write("create");
+   }else if(queryObject["action"]=="update"){
+res.write("update");
+   }else if(queryObject["action"]=="delete"){
+res.write("delete");
+   }
+     res.end();
+         //debugger;
+console.log(queryObject);
+}else{
+    fs(req.url,req,res);
+ }
+}
 console.log("Server running @http://localhost:8181/");
